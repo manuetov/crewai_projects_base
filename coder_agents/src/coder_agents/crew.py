ecba5efc06c71e3f8a72594ed94393e7b449303c
manuetov <manuetov@hotmail.com>
2026-02-22 21:40:53 +0100

    feat(config): incluir docs_engineer en estrategia del architect feat(crew): habilitar ejecución de código en test_engineer para correr tests

--- coder_agents/src/coder_agents/crew.py
+++ coder_agents/src/coder_agents/crew.py
@@ -123,6 +123,9 @@ class EngineeringTeam():
         return Agent(
             config=self.agents_config['test_engineer'],
             verbose=True,
+            allow_code_execution=True,
+            code_execution_mode="unsafe",
+            max_execution_time=600,
             max_retry_limit=5,
             tools=[McpFilesystemTool()],
         )
